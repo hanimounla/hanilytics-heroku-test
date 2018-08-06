804ea149f776106c05e4e9df4d94792130596cd8
hani mounla <hani mounla>
2018-08-06 10:00:26 +0300

    static

--- hanilytics/settings.py
+++ hanilytics/settings.py
@@ -122,3 +122,11 @@ TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files 
+)
