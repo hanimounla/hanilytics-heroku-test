16119b535cf4a4d08cb866146e9ad90feabdcaad
hani mounla <hani mounla>
2018-09-07 16:17:43 +0300

    debug

--- hanilytics/settings.py
+++ hanilytics/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p(by-ebo%c0(byc^l*#r@nbc^z1gd&js93$0!aau&h80f53o1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
